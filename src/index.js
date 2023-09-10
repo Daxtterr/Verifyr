7a4dccb291cd1afde9954b06e9357eee46203c73
Daxtterr <aimuelemmanuel@gmail.com>
2023-09-10 09:59:57 +0100

    fixed CORS

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
